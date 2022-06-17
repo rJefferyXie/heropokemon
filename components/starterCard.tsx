a5ab4cd2472b2e1f3f450d4b0212c67bfb74d768
JefferyXie <jeffxie27@outlook.com>
2022-06-17 17:44:38 -0600

    Styling the starter cards.

--- components/starterCard.tsx
+++ components/starterCard.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+// React and Styling
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/StarterCard.module.scss';
+
+// Constants
+import TypeColorSchemes from '../constants/TypeColorSchemes';
 
 interface StarterCardProps {
   name: string;
@@ -7,10 +11,30 @@ interface StarterCardProps {
 
 const StarterCard = (props: React.PropsWithChildren<StarterCardProps>) => {
   const { name } = props;
+  const [theme, setTheme] = useState('');
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    if (!name) return;
+
+    const pokedex = localStorage.getItem('kanto');
+    const artwork = localStorage.getItem('artwork') || 'official';
+    if (pokedex) {
+      const pokedexMap = JSON.parse(pokedex);
+      const pokemonInfo = pokedexMap[name];
+      const pokemonType = pokemonInfo.types[0];
+      const pokemonImage = pokemonInfo.sprites[artwork];
+      setTheme(TypeColorSchemes[pokemonType]);
+      setImage(pokemonImage);
+    }
+  }, [name]);
 
   return (
-    <div className={styles.container}>
-      {name}
+    <div className={styles.wrapper}>
+      <div className={styles.card} style={{backgroundColor: theme}}>
+        <img className={styles.cardImage} src={image} alt={`An image of " + ${name}`}></img>
+        <p className={styles.cardName}>{name}</p>
+      </div>
     </div>
   )
 }
