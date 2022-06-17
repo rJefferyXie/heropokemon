fcbb088a9b8a085b0cb5ec101601218c966361c8
JefferyXie <jeffxie27@outlook.com>
2022-06-17 17:27:03 -0600

    Refactored code.

--- components/starters.tsx
+++ components/starters.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+// React and Styling
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Starters.module.scss';
 
+// Constants
+import StarterPokemon from '../constants/StarterPokemon';
+
+// Components
 import StarterCard from './starterCard';
 
 const Starters = () => {
+  const [starterPokemon, setStarterPokemon] = useState<string[]>([]);
+
+  useEffect(() => {
+    const region = localStorage.getItem("region") || 'kanto';
+    setStarterPokemon(StarterPokemon[region]);
+  }, []);
+
   return (
     <div id="Starters" className={styles.container}>
-      <StarterCard name="grass"></StarterCard>
-      <StarterCard name="fire"></StarterCard>
-      <StarterCard name="water"></StarterCard>
+      {starterPokemon.map((pokemon, idx) => {
+        return <StarterCard name={pokemon} key={idx}></StarterCard>
+      })}
     </div>
   )
 }
