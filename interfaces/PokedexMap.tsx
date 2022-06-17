d1a6d04e49d0f63ece555be4bef44dadad149e08
JefferyXie <jeffxie27@outlook.com>
2022-06-17 17:26:46 -0600

    Interface for pokemon maps.

--- interfaces/PokedexMap.tsx
+++ interfaces/PokedexMap.tsx
@@ -1,4 +1,18 @@
-import PokemonMap from './PokemonMap';
+interface PokemonMap {
+  name: string,
+  height: number,
+  weight: number,
+  is_mythical: boolean,
+  is_legendary: boolean,
+  stats: number[],
+  types: string[],
+  evolutions: string[],
+  sprites: {
+    dream: string,
+    home: string,
+    official: string
+  }
+}
 
 interface PokedexMap {
   [key: string]: PokemonMap
