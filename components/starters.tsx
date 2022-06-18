660d17dd8d23d4621ec617a3c5d231246ccbfba2
JefferyXie <jeffxie27@outlook.com>
2022-06-17 20:20:34 -0600

    Can now change region.

--- components/starters.tsx
+++ components/starters.tsx
@@ -4,25 +4,30 @@ import styles from '../styles/Starters.module.scss';
 
 // Constants
 import StarterPokemon from '../constants/StarterPokemon';
+import RegionColorSchemes from '../constants/RegionColorSchemes';
 
 // Components
 import StarterCard from './starterCard';
+import Regions from './regions';
 
 const Starters = () => {
   const [starterPokemon, setStarterPokemon] = useState<string[]>([]);
+  const [region, setRegion] = useState('kanto');
 
   useEffect(() => {
-    const region = localStorage.getItem("region") || 'kanto';
     setStarterPokemon(StarterPokemon[region]);
-  }, []);
+  }, [region]);
 
   return (
     <div id="Starters" className={styles.container}>
-      <h1 className={styles.title}>Choose a starter!</h1>
-      <div className={styles.cardContainer}>
-        {starterPokemon.map((pokemon, idx) => {
-          return <StarterCard name={pokemon} key={idx}></StarterCard>
-        })}
+      <Regions setRegion={setRegion}></Regions>
+      <div className={styles.innerContainer} style={{backgroundColor: RegionColorSchemes[region]}}>
+        <h1 className={styles.title}>Choose a Starter!</h1>
+        <div className={styles.cardContainer}>
+          {starterPokemon.map((pokemon, idx) => {
+            return <StarterCard name={pokemon} key={idx}></StarterCard>
+          })}
+        </div>
       </div>
     </div>
   )
