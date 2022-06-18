13babd49c48272c40e7f9e6b6a59a240d522fc77
JefferyXie <jeffxie27@outlook.com>
2022-06-17 18:45:10 -0600

    Created currency component.

---
+++ components/currency.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../styles/Currency.module.scss';
+
+interface CurrencyProps {
+  currency: string;
+}
+
+const Currency = (props: React.PropsWithChildren<CurrencyProps>) => {
+  const { currency } = props;
+
+  return (
+    <div className={styles.container}>
+      <p>
+        $<span>{currency}</span>
+      </p>
+    </div>
+  )
+}
+
+export default Currency;
