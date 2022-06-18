b72bbe453ef8f677f2bcacfa911fa2e4efd084ac
JefferyXie <jeffxie27@outlook.com>
2022-06-17 18:45:02 -0600

    Created gameDetails component.

---
+++ components/gameDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/GameDetails.module.scss';
+
+const GameDetails = () => {
+  return (
+    <div className={styles.container}>
+      <p>hi</p>
+    </div>
+  )
+}
+
+export default GameDetails;
