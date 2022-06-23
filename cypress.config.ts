bef88ebbc21a27a7aa6fab964a8eab3c4d108bd1
JefferyXie <jeffxie27@outlook.com>
2022-06-23 13:43:12 -0600

    Using cypress instead of jest and enzyme.

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'gkg17r',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
