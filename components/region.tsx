7777328930e96248e62711b89eb322cc85505338
JefferyXie <jeffxie27@outlook.com>
2022-06-23 15:19:38 -0600

    Responsive region selection page.

--- components/region.tsx
+++ components/region.tsx
@@ -14,6 +14,8 @@ import PokedexMap from '../interfaces/PokedexMap';
 
 // Constants
 import StarterPokemon from '../constants/StarterPokemon';
+import StatMap from '../constants/StatMap';
+import TypeColorSchemes from '../constants/TypeColorSchemes';
 
 // Components
 import StarterCard from './starterCard';
@@ -30,12 +32,19 @@ const Region = (props: React.PropsWithChildren<RegionProps>) => {
   const [discoveredPokemon, setDiscoveredPokemon] = useState<string[]>([]);
   const [starter, setStarter] = useState("");
   const [artwork, setArtwork] = useState("");
+  const [theme, setTheme] = useState("");
   const { region, pokedex, unlocked, exit } = props;
 
   useEffect(() => {
     setStarterPokemon(StarterPokemon[region]);
     setStarter(StarterPokemon[region][Math.floor(Math.random() * 3)]);
   }, [region]);
+
+  useEffect(() => {
+    if (starter === "") return;
+
+    setTheme(TypeColorSchemes[pokedex[starter].types[0]]);
+  }, [starter, pokedex])
 
   useEffect(() => {
     const discovered = localStorage.getItem("discoveredPokemon") || '[]';
@@ -51,7 +60,6 @@ const Region = (props: React.PropsWithChildren<RegionProps>) => {
 
   const selectStarter = (pokemon: string) => {
     setStarter(pokemon);
-    console.log(pokedex["bulbasaur"]);
   }
 
   return (
@@ -73,14 +81,36 @@ const Region = (props: React.PropsWithChildren<RegionProps>) => {
                         key={idx}></StarterCard>
                       })}
                     </div>
-                    <p className={styles.selectedPokemon}>{"Selected Pokemon: " + starter}</p>
-                    <div className={styles.buttonContainer}>
-                      <Button variant="contained" onClick={reset} className={styles.exitButton}>EXIT</Button>
-                      <Button variant="contained" onClick={reset} className={styles.playButton}>PLAY</Button>
+                    <div className={styles.statsAndTypes}>
+                      <div className={styles.buttonContainer}>
+                        <Button variant="contained" onClick={reset} className={styles.exitButton}>EXIT</Button>
+                        <Button variant="contained" onClick={reset} className={styles.playButton}>PLAY</Button>
+                      </div>
+                      <div className={styles.typesAndSelected}>
+                        <strong className={styles.selectedPokemonName}>{starter}</strong>
+                        <img className={styles.selectedPokemonImage} src={pokedex[starter].sprites[artwork]} alt={"An image of " + starter}></img>
+                        <div className={styles.types}>
+                          {pokedex[starter].types.map((type, idx) => {
+                            return <p className={styles.type} key={idx} style={{backgroundColor: TypeColorSchemes[type]}}>{type}</p>
+                          })}
+                        </div>
+                      </div>
+                      <div className={styles.stats}>
+                        {pokedex[starter].stats.map((stat, idx) => {
+                          return <div className={styles.stat} key={idx}>
+                            <p className={styles.statName}>{StatMap[idx]}</p>
+                            <div className={styles.statBarWrapper}>
+                              <div className={styles.statBar} style={{width: stat / 1.5 + "%", backgroundColor: theme}}>
+                                <p className={styles.statValue}>{stat}</p>
+                              </div>
+                            </div>
+                          </div>
+                        })}
+                      </div>
                     </div>
                   </div>
                   <div className={styles.rightContainer}>
-                    <strong>Pokedex Entries</strong>
+                    <strong className={styles.pokedexHeader}>Pokedex Entries</strong>
                     {Object.keys(pokedex).sort().map((pokemon, idx) => {
                       return discoveredPokemon.includes(pokedex[pokemon].name) ? 
                       <div className={styles.pokedexEntry} key={idx}>
