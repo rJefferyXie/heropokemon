96c536fabb7ce90d80996f99419fae3b8e6f0e67
JefferyXie <jeffxie27@outlook.com>
2022-06-25 14:59:16 -0600

    Refactored enemy damage.

--- components/enemy.tsx
+++ components/enemy.tsx
@@ -1,5 +1,5 @@
 // React and Styling
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Enemy.module.scss';
 
 // Interfaces
@@ -7,25 +7,27 @@ import PokemonMap from '../interfaces/PokemonMap';
 
 interface EnemyProps {
   enemy: PokemonMap,
-  health: number,
-  takeDamage: Function,
+  takeDamage?: Function,
+  dealDamage?: Function,
+  clickDamage: number,
   artwork: string
 }
 
 const Enemy = (props: React.PropsWithChildren<EnemyProps>) => {
-  const { enemy, health, takeDamage, artwork } = props;
+  const { enemy, takeDamage, dealDamage, clickDamage, artwork } = props;
+  const [health, setHealth] = useState(enemy.stats[0]);
 
-  const hit = () => {
-    takeDamage();
+  const clickHit = () => {
+    setHealth(health - clickDamage);
   }
 
   return (
     <div className={styles.container}>
-      <img className={styles.enemyImage} src={enemy.sprites[artwork]} alt={"An image of " + enemy.name} onClick={hit}></img>
+      <img className={styles.enemyImage} src={enemy.sprites[artwork]} alt={"An image of " + enemy.name} onClick={clickHit}></img>
       <strong className={styles.enemyName}>{enemy.name}</strong>
       <p className={styles.enemyLevel}>{"LEVEL: " + enemy.level}</p>
       <div className={styles.healthBarWrapper}>
-        <div className={styles.healthBar} style={{width: Math.floor(health / enemy.stats[0] * 100) + "%", backgroundColor: "red"}}>
+        <div className={styles.healthBar} style={{width: Math.floor(health / enemy.stats[1] * 100) + "%"}}>
           <p className={styles.healthValue}>{Math.floor(health)}</p>
         </div>
       </div>
