40f928a15fbab8a317cb7d9160d0a38868782ed3
JefferyXie <jeffxie27@outlook.com>
2022-06-25 16:23:18 -0600

    Added health and heal.

--- components/pokemonCard.tsx
+++ components/pokemonCard.tsx
@@ -1,9 +1,12 @@
 // React and Styling
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/PokemonCard.module.scss';
 
 // MUI
 import { Button } from '@mui/material';
+
+// Constants
+import TypeColorSchemes from '../constants/TypeColorSchemes';
 
 // Interfaces
 import PokemonMap from '../interfaces/PokemonMap';
@@ -15,18 +18,31 @@ interface PokemonCardProps {
 
 const PokemonCard = (props: React.PropsWithChildren<PokemonCardProps>) => {
   const { pokemon, artwork } = props;
+  const [health, setHealth] = useState(pokemon.stats[0]);
+
+  const takeDamage = () => {
+    setHealth(health - 1);
+  }
+
+  const heal = () => {
+    setHealth(pokemon.stats[1]);
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.topRow}>
-        <img className={styles.pokemonImage} src={pokemon.sprites[artwork]} alt={"An image of " + pokemon.name}></img>
+        <img className={styles.pokemonImage} src={pokemon.sprites[artwork]} alt={"An image of " + pokemon.name} onClick={takeDamage}></img>
         <div className={styles.pokemonInfo}>
           <strong className={styles.pokemonName}>{pokemon.name}</strong>
           <p className={styles.pokemonLevel}>{"LEVEL: " + pokemon.level}</p>
         </div>
       </div>
       <div className={styles.bottomRow}>
-        <Button variant="contained">HEAL</Button>
+        <div className={styles.healthBarWrapper}>
+          <p className={styles.healthBar} style={{width: Math.floor(health / pokemon.stats[1] * 100) + "%"}}></p>
+          <p className={styles.healthValue}>{Math.floor(health)}</p>
+        </div>
+        <Button className={styles.healButton} variant="contained" onClick={heal}>HEAL</Button>
       </div>
     </div>
   )
