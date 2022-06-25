15bc9781e594a108f11edbb057de135b005fa92b
JefferyXie <jeffxie27@outlook.com>
2022-06-25 14:11:37 -0600

    Added navbar props.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,12 +5,30 @@ import styles from '../styles/Navbar.module.scss';
 // Components
 import Currency from './currency';
 import GameDetails from './gameDetails';
+import PokemonCard from './pokemonCard';
 
-const Navbar = () => {
+// Interfaces
+import PokedexMap from '../interfaces/PokedexMap';
+
+interface NavbarProps {
+  currency: number,
+  items: {},
+  storage: {},
+  team: PokedexMap,
+  badges: string[],
+  artwork: string
+}
+
+const Navbar = (props: React.PropsWithChildren<NavbarProps>) => {
+  const { currency, items, storage, team, badges, artwork } = props;
+
   return (
     <nav className={styles.container}>
-      <Currency currency={'0'}></Currency>
-      <GameDetails></GameDetails>
+      <Currency currency={currency}></Currency>
+      {Object.keys(team).map((pokemon, idx) => {
+        return <PokemonCard pokemon={team[pokemon]} artwork={artwork} key={idx}></PokemonCard>
+      })}
+      {/* <GameDetails></GameDetails> */}
     </nav>
   )
 }
