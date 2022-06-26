d50f794eca833a5c1c2b090a55fdfcc53fd84aa8
JefferyXie <jeffxie27@outlook.com>
2022-06-25 20:26:08 -0600

    Updating state in game file.

--- components/pokemonCard.tsx
+++ components/pokemonCard.tsx
@@ -13,25 +13,27 @@ import PokemonMap from '../interfaces/PokemonMap';
 
 interface PokemonCardProps {
   artwork: string,
-  pokemon: PokemonMap,
+  pokemon: PokemonMap
 }
 
 const PokemonCard = (props: React.PropsWithChildren<PokemonCardProps>) => {
   const { pokemon, artwork } = props;
   const [health, setHealth] = useState(pokemon.stats[0]);
 
-  const takeDamage = () => {
-    setHealth(health - 1);
+  const getHit = () => {
+    pokemon.stats[0] -= 2;
+    setHealth(health - 2);
   }
 
   const heal = () => {
+    pokemon.stats[0] = pokemon.stats[1];
     setHealth(pokemon.stats[1]);
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.topRow}>
-        <img className={styles.pokemonImage} src={pokemon.sprites[artwork]} alt={"An image of " + pokemon.name} onClick={takeDamage}></img>
+        <img className={styles.pokemonImage} src={pokemon.sprites[artwork]} alt={"An image of " + pokemon.name} onClick={getHit}></img>
         <div className={styles.pokemonInfo}>
           <strong className={styles.pokemonName}>{pokemon.name}</strong>
           <p className={styles.pokemonLevel}>{"LEVEL: " + pokemon.level}</p>
