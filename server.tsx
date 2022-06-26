39571ca00ecb30311b54bd24da9e0036efc45bc4
JefferyXie <jeffxie27@outlook.com>
2022-06-26 14:29:56 -0600

    Adding github secrets.

--- server.tsx
+++ server.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId
+  apiKey: process.env.NEXT_PUBLIC_apiKey || process.env.apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain || process.env.authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId || process.env.projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket || process.env.storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId || process.env.messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId || process.env.appId
 };
 
 const app = initializeApp(firebaseConfig);
