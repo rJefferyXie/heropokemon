d0890a794d975de7b618a110e25904a22735e102
JefferyXie <jeffxie27@outlook.com>
2022-07-01 17:11:26 -0600

    Added pokedex.

--- interfaces/GameSave.tsx
+++ interfaces/GameSave.tsx
@@ -1,10 +1,13 @@
+import PokedexMap from './PokedexMap';
+
 interface GameSave {
   "team": {},
   "items": {},
   "storage": {},
   "floor": number,
   "badges": string[],
-  "currency": number
+  "currency": number,
+  "pokedex": PokedexMap
 }
 
 export default GameSave;
