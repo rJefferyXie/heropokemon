33ee5ee7f0bb72cc563d8740c53c914fd2733ad3
JefferyXie <jeffxie27@outlook.com>
2022-07-05 19:44:36 -0600

    Removed inline styling.

--- components/options.tsx
+++ components/options.tsx
@@ -21,41 +21,36 @@ const Options = (props: React.PropsWithChildren<OptionsProps>) => {
   return (
     <div className={styles.container}>
       <Button 
-        className={styles.muiButton} 
+        className={selected === 0 ? styles.optionSelected : styles.option} 
         variant="contained" 
-        style={selected === 0 ? {backgroundColor: "#264E78"} : {backgroundColor: "#4C5A68"}}
         onClick={() => select(0)}>
         <CatchingPokemonIcon></CatchingPokemonIcon>
       </Button>
 
       <Button 
-        className={styles.muiButton} 
+        className={selected === 1 ? styles.optionSelected : styles.option} 
         variant="contained" 
-        style={selected === 1 ? {backgroundColor: "#264E78"} : {backgroundColor: "#4C5A68"}}
         onClick={() => select(1)}>
         <DesktopWindowsIcon></DesktopWindowsIcon>
       </Button>
 
       <Button 
-        className={styles.muiButton} 
+        className={selected === 2 ? styles.optionSelected : styles.option} 
         variant="contained" 
-        style={selected === 2 ? {backgroundColor: "#264E78"} : {backgroundColor: "#4C5A68"}}
         onClick={() => select(2)}>
         <CategoryIcon></CategoryIcon>
       </Button>
 
       <Button 
-        className={styles.muiButton} 
+        className={selected === 3 ? styles.optionSelected : styles.option} 
         variant="contained" 
-        style={selected === 3 ? {backgroundColor: "#264E78"} : {backgroundColor: "#4C5A68"}}
         onClick={() => select(3)}>        
         <ShoppingCartIcon></ShoppingCartIcon>
       </Button>
 
       <Button 
-        className={styles.muiButton} 
+        className={selected === 4 ? styles.optionSelected : styles.option} 
         variant="contained" 
-        style={selected === 4 ? {backgroundColor: "#264E78"} : {backgroundColor: "#4C5A68"}}
         onClick={() => select(4)}>
         <LeaderboardIcon></LeaderboardIcon>
       </Button>
