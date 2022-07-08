60314131f8a070653eb9c333f16e3d616bd7687e
JefferyXie <jeffxie27@outlook.com>
2022-07-08 16:59:30 -0600

    Reduced enemy damage.

--- gameFunctions/getDPS.tsx
+++ gameFunctions/getDPS.tsx
@@ -47,7 +47,7 @@ const getDPS = (enemy: PokemonMap, pokemon: PokemonMap) => {
 
   // make sure that both sides are doing more than 0 damage per interval
   playerDPS = Math.max(playerDPS, enemyHP) / 5;
-  enemyDPS = Math.max(enemyDPS, playerHP) / 15;
+  enemyDPS = Math.max(enemyDPS, playerHP) / 20;
 
   return { playerDPS, enemyDPS }
 }
