221e367094ba8d2a0cc13a56c99b900b73817d6b
JefferyXie <jeffxie27@outlook.com>
2022-07-11 16:52:11 -0600

    Added a persisted redux store.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+// Next and Styling
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+// Redux
+import { PersistGate } from 'redux-persist/integration/react';
+import { wrapper, store, persistor } from '../store/store';
+import { Provider } from 'react-redux';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps}/>
+      </PersistGate>
+    </Provider>
+  )
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
