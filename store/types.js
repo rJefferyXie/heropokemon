4e1a0bde9e2562b2e4e05364b508788b5b9eab63
JefferyXie <jeffxie27@outlook.com>
2022-07-12 00:40:31 -0600

    Added entry type.

--- store/types.js
+++ store/types.js
@@ -3,3 +3,4 @@ export const SET_POKEDEX = "SET_POKEDEX";
 export const SET_VISITED = "SET_VISITED";
 export const SET_LOADING = "SET_LOADING";
 export const ADD_REGION = "ADD_REGION";
+export const ADD_ENTRY = "ADD_ENTRY";
