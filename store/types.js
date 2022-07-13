7e7c7ddae4928206a80e8559e1aed865d0ea5a7b
JefferyXie <jeffxie27@outlook.com>
2022-07-12 19:49:06 -0600

    Added game and region types.

--- store/types.js
+++ store/types.js
@@ -2,5 +2,22 @@ export const CHANGE_ARTWORK = "CHANGE_ARTWORK";
 export const SET_POKEDEX = "SET_POKEDEX";
 export const SET_VISITED = "SET_VISITED";
 export const SET_LOADING = "SET_LOADING";
-export const ADD_REGION = "ADD_REGION";
 export const ADD_ENTRY = "ADD_ENTRY";
+
+export const UNLOCK_REGION = "UNLOCK_REGION";
+export const SET_REGION = "SET_REGION";
+
+// Game Types
+export const SET_CURRENT_FLOOR = "SET_CURRENT_FLOOR";
+export const SET_HIGHEST_FLOOR = "SET_HIGHEST_FLOOR";
+export const SET_ENEMIES_LEFT = "SET_ENEMIES_LEFT";
+export const SET_CLICK_DAMAGE = "SET_CLICK_DAMAGE";
+export const SET_PLAYER_DPS = "SET_PLAYER_DPS";
+export const SET_CURRENCY = "SET_CURRENCY";
+export const SET_STORAGE = "SET_STORAGE";
+export const SET_BADGES = "SET_BADGES";
+export const SET_ALERTS = "SET_ALERTS";
+export const SET_ITEMS = "SET_ITEMS";
+export const SET_ENEMY = "SET_ENEMY";
+export const HIT_ENEMY = "HIT_ENEMY";
+export const SET_TEAM = "SET_TEAM";
