70f5ab267675178efa63bcfa4019ba8ccdf4ea05
JefferyXie <jeffxie27@outlook.com>
2022-07-12 19:48:58 -0600

    Disabled serializable and immutable checks.

--- store/store.js
+++ store/store.js
@@ -27,9 +27,8 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      serializableCheck: false,
+      immutableCheck: false
   })}
 );
 
