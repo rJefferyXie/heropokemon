dd4d4b9a9e0dd3b31e156aebb6cbf30e0c7e4577
JefferyXie <jeffxie27@outlook.com>
2022-07-12 21:00:48 -0600

    Refactored types.

--- store/types.js
+++ store/types.js
@@ -1,26 +1,38 @@
+// Settings Types
 export const CHANGE_ARTWORK = "CHANGE_ARTWORK";
-export const SET_POKEDEX = "SET_POKEDEX";
 export const SET_VISITED = "SET_VISITED";
 export const SET_LOADING = "SET_LOADING";
+
+// Pokedex Types
+export const SET_POKEDEX = "SET_POKEDEX";
 export const ADD_ENTRY = "ADD_ENTRY";
 
+// Regions Types
 export const UNLOCK_REGION = "UNLOCK_REGION";
 export const SET_REGION = "SET_REGION";
 
 // Game Types
-export const SET_CURRENT_FLOOR = "SET_CURRENT_FLOOR";
-export const SET_HIGHEST_FLOOR = "SET_HIGHEST_FLOOR";
-export const SET_ENEMIES_LEFT = "SET_ENEMIES_LEFT";
-export const SET_CLICK_DAMAGE = "SET_CLICK_DAMAGE";
-export const SET_PLAYER_DPS = "SET_PLAYER_DPS";
 export const SET_CURRENCY = "SET_CURRENCY";
 export const SET_STORAGE = "SET_STORAGE";
 export const SET_BADGES = "SET_BADGES";
 export const SET_ITEMS = "SET_ITEMS";
 
+// Damage Types
+export const SET_CLICK_DAMAGE = "SET_CLICK_DAMAGE";
+export const SET_PLAYER_DPS = "SET_PLAYER_DPS";
+
+// Floor Types
+export const SET_CURRENT_FLOOR = "SET_CURRENT_FLOOR";
+export const SET_HIGHEST_FLOOR = "SET_HIGHEST_FLOOR";
+
+// Alert Types
 export const ADD_ALERT = "ADD_ALERT";
 export const NEXT_ALERT = "NEXT_ALERT";
 
+// Enemy Types
+export const SET_ENEMIES_LEFT = "SET_ENEMIES_LEFT";
 export const SET_ENEMY = "SET_ENEMY";
 export const HIT_ENEMY = "HIT_ENEMY";
+
+// Team Types
 export const SET_TEAM = "SET_TEAM";
