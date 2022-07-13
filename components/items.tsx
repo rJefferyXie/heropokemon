30dd09ebc4d95883a274663db496eff751ee2800
JefferyXie <jeffxie27@outlook.com>
2022-07-13 16:27:26 -0600

    Created empty item and shop files.

---
+++ components/items.tsx
@@ -0,0 +1,11 @@
+// React and Styling
+import React from 'react';
+import styles from '../styles/Items.module.scss';
+
+const Items = () => {
+  return (
+    <div>Items</div>
+  )
+}
+
+export default Items;
---
+++ components/shop.tsx
@@ -0,0 +1,11 @@
+// React and Styling
+import React from 'react';
+import styles from '../styles/Shop.module.scss';
+
+const Shop = () => {
+  return (
+    <div>Shop</div>
+  )
+}
+
+export default Shop;
