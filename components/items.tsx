077a38e3a6bcbfe651e0c8306472688fee3fc6d1
JefferyXie <jeffxie27@outlook.com>
2022-07-14 17:46:15 -0600

    Can now use basic potions.

--- components/items.tsx
+++ components/items.tsx
@@ -1,6 +1,20 @@
 // React and Styling
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Items.module.scss';
+
+// Interfaces
+import ShopItem from '../interfaces/ShopItem';
+import PokemonMap from '../interfaces/PokemonMap';
+
+// Components
+import PokemonPreview from './pokemonPreview';
+
+// MUI
+import { ClickAwayListener, Button } from '@mui/material';
+
+// Animations
+import { motion, AnimatePresence } from 'framer-motion';
+import DropInTop from '../animations/dropInTop';
 
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
@@ -8,12 +22,81 @@ import allActions from '../store/actions/allActions';
 
 const Items = () => {
   const dispatch = useDispatch();
-  const items = useSelector((state: any) => {return state.itemReducer.items})
+  const items = useSelector((state: any) => {return state.itemReducer.items});
+  const team: PokemonMap[] = useSelector((state: any) => {return state.teamReducer.team});
+
+  const [item, setItem] = useState<ShopItem>();
+
+  const itemUse = (pokemonIdx: number) => {
+    if (!item || !item.quantity || item.quantity === 0) {
+      exit();
+      return;
+    }
+    
+    if (item.name === "Potion") {
+      if (team[pokemonIdx].stats[0] >= team[pokemonIdx].stats[1]) return;
+
+      const newTeam = JSON.parse(JSON.stringify(team));
+      const newItems = JSON.parse(JSON.stringify(items));
+      newItems["potion1"].quantity -= 1;
+      item.quantity -= 1;
+
+      if (newTeam[pokemonIdx].stats[0] + 20 >= newTeam[pokemonIdx].stats[1]) {
+        newTeam[pokemonIdx].stats[0] = newTeam[pokemonIdx].stats[1];
+      } else {
+        newTeam[pokemonIdx].stats[0] += 20;
+      }
+
+      dispatch(allActions.itemActions.setItems(newItems));
+      dispatch(allActions.teamActions.setTeam(newTeam));
+    }
+  }
+
+  const selectItem = (selectedItem: ShopItem) => {
+    setItem(selectedItem);
+  }
+
+  const exit = () => {
+    setItem(undefined);
+  }
 
   return (
     <div className={styles.container}>
+        <AnimatePresence>
+        {item !== undefined &&
+          <div className={styles.overlay}>
+            <ClickAwayListener onClickAway={exit}>
+              <motion.div className={styles.itemPreview} 
+                key="modal" 
+                initial="hidden" 
+                animate="visible" 
+                exit="exit" 
+                variants={DropInTop}
+                >
+                <div className={styles.previewRow}>
+                  <div className={styles.itemColPreview}>
+                    <img className={styles.imagePreview} src={item.image} alt={item.name}></img>
+                    <p className={styles.namePreview}>{item.name}</p>
+                    <p className={styles.quantityPreview}>{"Quantity: " + item.quantity}</p>
+                    <p className={styles.descriptionPreview}>{item.description}</p>
+                    <Button className={styles.exitButton} variant="contained" onClick={exit}>CANCEL</Button>
+                  </div>
+                  <div className={styles.infoColPreview}>
+                    <div className={styles.teamRow}>
+                      {team.map((pokemon, idx) => {
+                        return <PokemonPreview pokemon={pokemon} teamIdx={idx} itemUse={itemUse} key={idx}></PokemonPreview>
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            </ClickAwayListener>
+          </div>
+        }
+      </AnimatePresence>
+
       {Object.keys(items).map((item, idx) => {
-        return items[item].quantity > 0 && <div className={styles.item} key={idx}>
+        return items[item].quantity > 0 && <div className={styles.item} onClick={() => selectItem(items[item])} key={idx}>
           <img className={styles.itemImage} src={items[item].image} alt={items[item].name}></img>
           <div className={styles.infoCol}>
             <div className={styles.nameAndQuantity}>
