760b90fa5d86e043f07f58fa33f58dde6df8db45
JefferyXie <jeffxie27@outlook.com>
2022-07-14 16:11:31 -0600

    Refactored code so we only loop once.

--- gameFunctions/getDPS.tsx
+++ gameFunctions/getDPS.tsx
@@ -6,34 +6,33 @@ import PokemonMap from '../interfaces/PokemonMap';
 
 const getDPS = (enemy: PokemonMap, pokemon: PokemonMap) => {
   /* 
-    Damage calculation taken from official pokemon games from Gen IV and onwards.
+    Damage calculation taken from official pokemon games from Gen IV.
     link: https://bulbapedia.bulbagarden.net/wiki/Damage
-    stats[2] = attack
-    stats[3] = defense
-    stats[4] = special attack
-    stats[5] = special defense
-    stats[6] = speed
+
+    StatMap: 
+      stats[2] = attack
+      stats[3] = defense
+      stats[4] = special attack
+      stats[5] = special defense
+      stats[6] = speed
   */
   let playerDPS = (((((2 * pokemon.level) / 5) + 2) * pokemon.level * ((pokemon.stats[2] + pokemon.stats[4]) / (enemy.stats[3] + enemy.stats[5]))) / 50) + pokemon.stats[6] * 0.05;
   let enemyDPS = (((((2 * enemy.level) / 5) + 2) * enemy.level * ((enemy.stats[2] + enemy.stats[4]) / (pokemon.stats[3] + pokemon.stats[5]))) / 50) + enemy.stats[6] * 0.05;
 
-  // calculate player multipliers from type advantages or disadvantages
   for (let i = 0; i < pokemon.types.length; i++) {
-    const typeAdvantages = TypeAdvantages[pokemon.types[i]];
+    const playerTypeAdvantages = TypeAdvantages[pokemon.types[i]];
     for (let j = 0; j < enemy.types.length; j++) {
-      if (typeAdvantages.strong.includes(enemy.types[j])) playerDPS *= 2;
-      if (typeAdvantages.weak.includes(enemy.types[j])) playerDPS *= 0.5;
-      if (typeAdvantages.resist.includes(enemy.types[j])) playerDPS *= 0;          
-    }
-  }
+      const enemyTypeAdvantages = TypeAdvantages[enemy.types[i]];
 
-  // calculate enemy multipliers from type advantages or disadvantages
-  for (let i = 0; i < enemy.types.length; i++) {
-    const typeAdvantages = TypeAdvantages[enemy.types[i]];
-    for (let j = 0; j < pokemon.types.length; j++) {
-      if (typeAdvantages.strong.includes(pokemon.types[j])) enemyDPS *= 2;
-      if (typeAdvantages.weak.includes(pokemon.types[j])) enemyDPS *= 0.5;
-      if (typeAdvantages.resist.includes(pokemon.types[j])) enemyDPS *= 0;          
+      // calculate enemy multipliers from type advantages or disadvantages
+      if (enemyTypeAdvantages.strong.includes(pokemon.types[j])) enemyDPS *= 2;
+      if (enemyTypeAdvantages.weak.includes(pokemon.types[j])) enemyDPS *= 0.5;
+      if (enemyTypeAdvantages.resist.includes(pokemon.types[j])) enemyDPS *= 0;  
+
+      // calculate player multipliers from type advantages or disadvantages
+      if (playerTypeAdvantages.strong.includes(enemy.types[j])) playerDPS *= 2;
+      if (playerTypeAdvantages.weak.includes(enemy.types[j])) playerDPS *= 0.5;
+      if (playerTypeAdvantages.resist.includes(enemy.types[j])) playerDPS *= 0;          
     }
   }
 
