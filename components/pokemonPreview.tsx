651fe2d97d854fb7ba08ee0c4fad36991002d2bc
JefferyXie <jeffxie27@outlook.com>
2022-07-14 17:45:41 -0600

    Using pokemon preview card for item screen.

--- components/pokemonPreview.tsx
+++ components/pokemonPreview.tsx
@@ -9,18 +9,33 @@ import PokemonMap from '../interfaces/PokemonMap';
 import { useSelector } from 'react-redux';
 
 interface PokemonPreviewProps {
-  pokemon: PokemonMap
+  pokemon: PokemonMap,
+  teamIdx?: number,
+  itemUse?: Function
 }
 
 const PokemonPreview = (props: React.PropsWithChildren<PokemonPreviewProps>) => {
-  const { pokemon } = props;
+  const { pokemon, teamIdx, itemUse } = props;
   const artwork = useSelector((state: any) => {return state.settingReducer.artwork});
 
+  const handleClick = () => {
+    if (teamIdx !== undefined && teamIdx >= 0 && itemUse) {
+      itemUse(teamIdx);
+    }
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={teamIdx !== undefined && teamIdx >= 0 ? styles.containerItem : styles.container} onClick={handleClick}>
       <img className={styles.image} src={pokemon.sprites[artwork]} alt={"An image of " + pokemon.name}></img>
       <p className={styles.text}>{pokemon.name}</p>
-      <p className={styles.text}>{"LEVEL " + pokemon.level}</p>
+      {!teamIdx && teamIdx !== 0 && <p className={styles.text}>{"LEVEL " + pokemon.level}</p>}
+      
+      {teamIdx !== undefined && teamIdx >= 0 && <div className={styles.healthBarWrapper}>
+        <div className={styles.healthBar} style={{width: Math.floor(pokemon.stats[0] / pokemon.stats[1] * 100) + "%"}}>
+          <p className={styles.healthValue}>{Math.floor(pokemon.stats[0])}/{pokemon.stats[1]}</p>
+        </div>
+      </div>}
+
     </div>
   )
 }
