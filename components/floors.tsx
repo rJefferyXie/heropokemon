3bf9582574ee6ce33c6e6850108f8532b3383d3d
JefferyXie <jeffxie27@outlook.com>
2022-07-18 15:50:50 -0600

    Only using one reducer instance.

--- components/floors.tsx
+++ components/floors.tsx
@@ -13,11 +13,10 @@ import allActions from '../store/actions/allActions';
 
 const Floors = () => {
   const dispatch = useDispatch();
-  const floor = useSelector((state: any) => {return state.gameReducer.currentFloor});
-  const highestFloor = useSelector((state: any) => {return state.gameReducer.highestFloor});
+  const floor = useSelector((state: any) => {return state.gameReducer});
 
   const setFloor = (newFloor: number) => {
-    if (newFloor === floor) return;
+    if (newFloor === floor.currentFloor) return;
     dispatch(allActions.gameActions.setCurrentFloor(newFloor));
   }
 
@@ -26,48 +25,48 @@ const Floors = () => {
       {
         [...Array(5)].map((_, idx) => {
           {
-            return idx === 0 && floor > 2 ? 
-            <div className={styles.floor} key={idx} onClick={() => (setFloor(floor - 2))}>
-              <p>{floor - 2}</p>
+            return idx === 0 && floor.currentFloor > 2 ? 
+            <div className={styles.floor} key={idx} onClick={() => (setFloor(floor.currentFloor - 2))}>
+              <p>{floor.currentFloor - 2}</p>
               <div className={styles.iconContainer}>
                 <DoneIcon></DoneIcon>
-                {(floor - 2) % 10 === 0 && <TokenIcon></TokenIcon>}
+                {(floor.currentFloor - 2) % 10 === 0 && <TokenIcon></TokenIcon>}
               </div>
             </div> :
 
-            idx === 1 && floor > 1 ? 
-            <div className={styles.floor} key={idx} onClick={() => (setFloor(floor - 1))}>
-              <p>{floor - 1}</p>
+            idx === 1 && floor.currentFloor > 1 ? 
+            <div className={styles.floor} key={idx} onClick={() => (setFloor(floor.currentFloor - 1))}>
+              <p>{floor.currentFloor - 1}</p>
               <div className={styles.iconContainer}>
                 <DoneIcon></DoneIcon>
-                {(floor - 1) % 10 === 0 && <TokenIcon></TokenIcon>}
+                {(floor.currentFloor - 1) % 10 === 0 && <TokenIcon></TokenIcon>}
               </div>
             </div> :
 
             idx === 2 ? 
             <div className={styles.currentFloor} key={idx}>
-              <p>{floor}</p>
+              <p>{floor.currentFloor}</p>
               <div className={styles.iconContainer}>
                 <DoneIcon></DoneIcon>
-                {floor % 10 === 0 && <TokenIcon></TokenIcon>}
+                {floor.currentFloor % 10 === 0 && <TokenIcon></TokenIcon>}
               </div>
             </div> :
 
-            idx === 3 && floor < 100 ? 
-            <div className={styles.floor} key={idx} onClick={floor + 1 <= highestFloor ? () => (setFloor(floor + 1)) : () => null}>
-              <p>{floor + 1}</p>
+            idx === 3 && floor.currentFloor < 100 ? 
+            <div className={styles.floor} key={idx} onClick={floor.currentFloor + 1 <= floor.highestFloor ? () => (setFloor(floor.currentFloor + 1)) : () => null}>
+              <p>{floor.currentFloor + 1}</p>
               <div className={styles.iconContainer}>
-                {highestFloor >= floor + 1 ? <DoneIcon></DoneIcon> : <ClearIcon></ClearIcon>}
-                {(floor + 1) % 10 === 0 && <TokenIcon></TokenIcon>}
+                {floor.highestFloor >= floor.currentFloor + 1 ? <DoneIcon></DoneIcon> : <ClearIcon></ClearIcon>}
+                {(floor.currentFloor + 1) % 10 === 0 && <TokenIcon></TokenIcon>}
               </div>
             </div> :
             
-            idx === 4 && floor < 99 ? 
-            <div className={styles.floor} key={idx} onClick={floor + 2 <= highestFloor ? () => (setFloor(floor + 2)) : () => null}>
-              <p>{floor + 2}</p>
+            idx === 4 && floor.currentFloor < 99 ? 
+            <div className={styles.floor} key={idx} onClick={floor.currentFloor + 2 <= floor.highestFloor ? () => (setFloor(floor.currentFloor + 2)) : () => null}>
+              <p>{floor.currentFloor + 2}</p>
               <div className={styles.iconContainer}>
-                {highestFloor >= floor + 2 ? <DoneIcon></DoneIcon> : <ClearIcon></ClearIcon>}
-                {(floor + 2) % 10 === 0 && <TokenIcon></TokenIcon>}
+                {floor.highestFloor >= floor.currentFloor + 2 ? <DoneIcon></DoneIcon> : <ClearIcon></ClearIcon>}
+                {(floor.currentFloor + 2) % 10 === 0 && <TokenIcon></TokenIcon>}
               </div>
             </div> : null
           }
