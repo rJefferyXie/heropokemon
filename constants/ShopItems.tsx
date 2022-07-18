e32f9f3432b2745388c36b9091cbfedb4fe0b199
JefferyXie <jeffxie27@outlook.com>
2022-07-18 15:51:13 -0600

    Added vitamins and rare candies.

--- constants/ShopItems.tsx
+++ constants/ShopItems.tsx
@@ -29,6 +29,55 @@ const ShopItems: ShopItem[] = [
     cost: 5000,
     image: "images/potion4.png",
     description: "Fully heal a pokemon's hitpoints."
+  },
+  {
+    id: "hp",
+    name: "HP Up",
+    cost: 10000,
+    image: "images/hpUp.webp",
+    description: "Raise a pokemon's HP stat by 1."
+  },
+  {
+    id: "protein",
+    name: "Protein",
+    cost: 10000,
+    image: "images/protein.webp",
+    description: "Raise a pokemon's attack stat by 1."
+  },
+  {
+    id: "iron",
+    name: "Iron",
+    cost: 10000,
+    image: "images/iron.webp",
+    description: "Raise a pokemon's defense stat by 1."
+  },
+  {
+    id: "calcium",
+    name: "Calcium",
+    cost: 10000,
+    image: "images/calcium.webp",
+    description: "Raise a pokemon's special attack stat by 1."
+  },
+  {
+    id: "zinc",
+    name: "Zinc",
+    cost: 10000,
+    image: "images/zinc.webp",
+    description: "Raise a pokemon's special defense stat by 1."
+  },
+  {
+    id: "carbos",
+    name: "Carbos",
+    cost: 10000,
+    image: "images/carbos.webp",
+    description: "Raise a pokemon's speed stat by 1."
+  },
+  {
+    id: "rareCandy",
+    name: "Rare Candy",
+    cost: 25000,
+    image: "images/rareCandy.webp",
+    description: "Raise a pokemon's level by 1."
   }
 ]
 
