81c551432fadedc2bfe2e6e93018f41139942b15
JefferyXie <jeffxie27@outlook.com>
2022-07-19 16:59:32 -0600

    Started bonuses component and styles.

---
+++ components/bonuses.tsx
@@ -0,0 +1,13 @@
+// React and Styling
+import React from 'react';
+import styles from '../styles/Bonuses.module.scss';
+
+const Bonuses = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Bonuses;
