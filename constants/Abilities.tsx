6aa3af3a823c56100bc44179213be5eac451b5aa
JefferyXie <jeffxie27@outlook.com>
2022-07-21 12:58:51 -0600

    Changed numbers and strings to functions.

--- constants/Abilities.tsx
+++ constants/Abilities.tsx
@@ -1,68 +1,122 @@
 // Interfaces
 import Ability from '../interfaces/Ability';
 
+// Constants
+import AbilityLevels from './AbilityLevels';
+
 const Abilities: Ability[] = [
   {
     id: "regeneration",
-    name: "Regeneration I",
+    name: (level: number) => {
+      return "Regeneration " + AbilityLevels[level];
+    },
     level: 0,
-    cost: 3,
+    cost: (level: number) => {
+      return level ** 2 + 2
+    },
     image: "/images/lightBall.webp",
-    description: "Pokemon on your team with more than 0 HP will regenerate 1 HP per second."
+    description: (level: number) => {
+      return `Pokemon on your team with more than 0 HP will regenerate ${level ** 2} HP per second.`;
+    }
   },
   {
     id: "kindSoul",
-    name: "Kind Soul I",
+    name: (level: number) => {
+      return "Kind Soul " + AbilityLevels[level];
+    },
     level: 0,
-    cost: 3,
+    cost: (level: number) => {
+      return level ** 2 + 2
+    },
     image: "/images/heartScale.webp",
-    description: "All wild pokemon are 10% more likely to join your team."
+    description: (level: number) => {
+      return `All wild pokemon are ${level * 10}% more likely to join your team.`;
+    }
   },
   {
     id: "vigor",
-    name: "Vigor I",
+    name: (level: number) => {
+      return "Vigor " + AbilityLevels[level];
+    },
     level: 0,
-    cost: 3,
+    cost: (level: number) => {
+      return level ** 2 + 2
+    },
     image: "/images/deepSeaTooth.webp",
-    description: "Your pokemon deal 10% more damage."
+    description: (level: number) => {
+      return `Your pokemon deal ${level * 10}% more damage.`;
+    }
   },
   {
     id: "strongStyle",
-    name: "Strong Style I",
+    name: (level: number) => {
+      return "Strong Style " + AbilityLevels[level];
+    },
     level: 0,
-    cost: 3,
+    cost: (level: number) => {
+      return level ** 2 + 2
+    },
     image: "/images/blackBelt.webp",
-    description: "Your clicks now deal 2 damage."
+    description: (level: number) => {
+      return `Your clicks deal ${level ** 2 + 1} damage to wild pokemon.`;
+    }
   },
   {
     id: "fortune",
-    name: "Fortune I",
+    name: (level: number) => {
+      return "Fortune " + AbilityLevels[level];
+    },
     level: 0,
-    cost: 3,
+    cost: (level: number) => {
+      return level ** 2 + 2
+    },
     image: "/images/amuletCoin.webp",
-    description: "Wild pokemon drop 10% more currency."
+    description: (level: number) => {
+      return `Wild pokemon drop ${level * 10}% more currency.`;
+    }
   },
   {
     id: "mysteryBall",
-    name: "Mystery Ball",
-    cost: 10,
+    name: (_: any) => {
+      return "Mystery Ball"
+    },
+    level: -1,
+    cost: (_: any) => {
+      return 7;
+    },
     image: "/images/pokeball.webp",
-    description: "Adds a random and unique pokemon to your team."
+    description: (_: any) => {
+      return "Adds a random and unique pokemon to your team or storage.";
+    }
   },
   {
     id: "swapper",
-    name: "Auto Swapper",
-    cost: 25,
+    name: (_: any) => {
+      return "Auto Swapper"
+    },
+    level: -1,
+    cost: (_: any) => {
+      return 20;
+    },
     image: "/images/lustrousOrb.webp",
-    description: "Pokemon on your team will be swapped to the first slot automatically."
+    description: (_: any) => {
+      return "Pokemon on your team will be swapped to the first slot automatically.";
+    } 
   },
   {
     id: "healer",
-    name: "Auto Healer",
-    cost: 25,
+    name: (_: any) => {
+      return "Auto Healer";
+    },
+    level: -1,
+    cost: (_: any) => {
+      return 20;
+    },
     image: "/images/soulDew.png",
-    description: "Automatically uses your potions on your pokemon when they are low on HP."
-  },
+    description: (_: any) => {
+      return "Automatically uses your potions on your pokemon when their health gets low.";
+    }
+  }
 ]
 
 export default Abilities;
