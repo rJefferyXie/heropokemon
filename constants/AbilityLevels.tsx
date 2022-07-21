8b9ba798a1f39334c516ca3c4ac510a78b3c7a5a
JefferyXie <jeffxie27@outlook.com>
2022-07-21 12:58:34 -0600

    Levels correlate to roman numerals.

---
+++ constants/AbilityLevels.tsx
@@ -0,0 +1,16 @@
+interface AbilityLevelMap {
+  [key: number]: string
+}
+
+const AbilityLevels: AbilityLevelMap = {
+  0: "",
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII"
+}
+
+export default AbilityLevels;
