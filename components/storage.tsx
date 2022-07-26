89edd96ac246278828c2d546923d9760396a2c16
JefferyXie <jeffxie27@outlook.com>
2022-07-26 15:53:34 -0600

    Implemented pagination.

--- components/storage.tsx
+++ components/storage.tsx
@@ -6,6 +6,9 @@ import styles from '../styles/Storage.module.scss';
 import StorageCard from './storageCard';
 import SwapScreen from './swapScreen';
 
+// MUI
+import { Pagination } from '@mui/material';
+
 // Interfaces
 import PokemonMap from '../interfaces/PokemonMap';
 
@@ -20,6 +23,11 @@ const Storage = () => {
 
   const [swappingIdx, setSwappingIdx] = useState(-1);
   const [swapping, setSwapping] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const handleChange = (_: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   const handleSwap = (swapIdx: number) => {
     const teamCopy = JSON.parse(JSON.stringify(team));
@@ -45,15 +53,27 @@ const Storage = () => {
         </SwapScreen>
       }
 
-      {storage.slice(0, 6).map((pokemon, idx) => {
+      {storage.slice((page - 1) * 6, page * 6).map((pokemon, idx) => {
         return <StorageCard 
           pokemon={pokemon} 
-          index={idx} 
+          index={idx + (page - 1) * 6} 
           setSwapping={setSwapping} 
           setSwappingIdx={setSwappingIdx} 
           key={idx}>
           </StorageCard>
       })}
+
+      <Pagination 
+        className={styles.pagination} 
+        count={Math.ceil(storage.length / 6)} 
+        onChange={handleChange}
+        siblingCount={1}
+        boundaryCount={0}
+        sx={{button:{color: '#ffffff', borderColor: '#ffffff'}}}
+        color="primary"
+        variant="outlined"         
+        shape="rounded" 
+      />
     </div>
   );
 }
