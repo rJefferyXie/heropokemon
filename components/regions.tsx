f0863a4ddd375a018cf025d584a6e0082be04bdd
Jeffery Xie <jeffxie27@outlook.com>
2022-08-17 16:19:51 -0600

    Added a quick play button.

--- components/regions.tsx
+++ components/regions.tsx
@@ -6,51 +6,67 @@ import styles from '../styles/Regions.module.scss';
 import RegionPreview from './regionPreview';
 import Region from './region';
 
+// MUI
+import { Button } from '@mui/material';
+
 // Redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import allActions from '../store/actions/allActions';
 
 const Regions = () => {
+  const dispatch = useDispatch();
   const selected = useSelector((state: any) => {return state.regionsReducer.selected});
   const visited = useSelector((state: any) => {return state.settingReducer.visited});
+  const regions = useSelector((state: any) => {return state.regionsReducer.regions});
   
+  const selectRegion = () => {
+    const region = regions[Math.floor(Math.random() * (regions.length - 1))];
+    dispatch(allActions.regionsActions.setRegion(region));
+  }
+
   return (
     <div className={!visited ? styles.containerHidden : styles.container}>
       {selected && <RegionPreview></RegionPreview>}
 
-      <Region 
-        name="kanto"
-        images={["venusaur", "charizard", "blastoise", "articuno", "zapdos", "moltres"]}>
-      </Region>
+      <h1 className={styles.regionTitle}>HeroPokemon</h1>
+      <Button className={styles.playButton} variant="contained" onClick={selectRegion}>QUICK PLAY</Button>
 
-      <Region 
-        name="johto" 
-        images={["meganium", "typhlosion", "feraligatr", "ho-oh", "lugia", "celibi"]}>
-      </Region>
+      <div className={styles.regionContainer}>
+        <Region 
+          name="kanto"
+          images={["venusaur", "charizard", "blastoise", "articuno", "zapdos", "moltres"]}>
+        </Region>
 
-      <Region 
-        name="hoenn"
-        images={["sceptile", "blaziken", "swampert", "groudon", "kyogre", "rayquaza"]}>
-      </Region>
+        <Region 
+          name="johto" 
+          images={["meganium", "typhlosion", "feraligatr", "ho-oh", "lugia", "celibi"]}>
+        </Region>
 
-      <Region 
-        name="sinnoh" 
-        images={["torterra", "infernape", "empoleon", "dialga", "palkia", "giratina"]}>
-      </Region>
+        <Region 
+          name="hoenn"
+          images={["sceptile", "blaziken", "swampert", "groudon", "kyogre", "rayquaza"]}>
+        </Region>
 
-      <Region 
-        name="unova"
-        images={["serperior", "emboar", "samurott", "reshiram", "zekrom", "kyurem"]}>
-      </Region>
+        <Region 
+          name="sinnoh" 
+          images={["torterra", "infernape", "empoleon", "dialga", "palkia", "giratina"]}>
+        </Region>
 
-      <Region 
-        name="kalos"
-        images={["chesnaught", "delphox", "greninja", "xerneas", "yveltal", "zygarde"]}>
-      </Region>
+        <Region 
+          name="unova"
+          images={["serperior", "emboar", "samurott", "reshiram", "zekrom", "kyurem"]}>
+        </Region>
 
-      <Region 
-        name="alola"
-        images={["decidueye", "incineroar", "primarina", "lunala", "solgaleo", "necrozma"]}>
-      </Region>
+        <Region 
+          name="kalos"
+          images={["chesnaught", "delphox", "greninja", "xerneas", "yveltal", "zygarde"]}>
+        </Region>
+
+        <Region 
+          name="alola"
+          images={["decidueye", "incineroar", "primarina", "lunala", "solgaleo", "necrozma"]}>
+        </Region>
+      </div>
     </div>
   );
 }
