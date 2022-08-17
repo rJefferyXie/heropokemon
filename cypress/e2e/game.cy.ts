c379042845e121ee673038278e3ba461d14cb73f
Jeffery Xie <jeffxie27@outlook.com>
2022-08-17 13:54:51 -0600

    Tested the majority of the game actions.

--- cypress/e2e/game.cy.ts
+++ cypress/e2e/game.cy.ts
@@ -16,24 +16,15 @@ describe('Testing the whole game including all items, buttons, and abilities.', 
     cy.get('.css-sghohy-MuiButtonBase-root-MuiButton-root').eq(1).should('be.visible').click();
 
     // Wait for page to redirect
-    cy.wait(5000);
+    cy.wait(1500);
   });
 
   describe('A full playthrough of the Kanto region.', () => {
     it('Modify currency and bonus points to test the shop and abilities.', () => {
       cy.window().its('store').invoke('dispatch', { type: 'SET_CURRENCY', payload: 99999 });
       cy.window().its('store').invoke('dispatch', { type: 'SET_BONUS_POINTS', payload: 99999 });
+      cy.window().its('store').invoke('dispatch', { type: 'SET_HIGHEST_FLOOR', payload: 4 });    
       cy.window().its('store').invoke('dispatch', { type: 'SET_CURRENT_FLOOR', payload: 5 });
-      cy.window().its('store').invoke('dispatch', { type: 'SET_HIGHEST_FLOOR', payload: 10 });
-      cy.window().its('store').invoke('dispatch', { type: 'SET_CURRENT_FLOOR', payload: 1 });
-    
-      // swapping pokemon with first in order (2, 3, 4, 5, 6)
-        
-      // using every item from item page
-  
-      // purchasing every ability
-  
-      // change floor
     });
 
     it('Healing the first pokemon.', () => {
@@ -198,6 +189,61 @@ describe('Testing the whole game including all items, buttons, and abilities.', 
         cy.get('.PokemonPreview_containerItem__TMOMh').eq(0).click();
         cy.window().its('store').invoke('getState').its('itemReducer').its('items').its('rareCandy').its('quantity').should('deep.equal', 0);
         cy.get('.Items_exitButton__5S7p3').eq(0).click();
+      });
+    });
+
+    describe('Purchasing every ability.', () => {
+      it('Buying Regeneration.', () => {
+        cy.get('.Options_option__h4v8l').eq(3).click();
+        cy.get('.Bonuses_ability__QY6P3').eq(0).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('regeneration').its('level').should('deep.equal', 1);
+      });
+
+      it('Buying Kind Soul.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(1).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('kindSoul').its('level').should('deep.equal', 1);
+      });
+
+      it('Buying Vigor.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(2).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('vigor').its('level').should('deep.equal', 1);
+      });
+
+      it('Buying Strong Style.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(3).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('strongStyle').its('level').should('deep.equal', 1);
+      });
+
+      it('Buying Fortune.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(4).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('fortune').its('level').should('deep.equal', 1);
+      });
+
+      it('Buying Swapper.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(5).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('swapper').its('unlocked').should('deep.equal', true);
+      });
+
+      it('Buying Healer.', () => {
+        cy.get('.Bonuses_ability__QY6P3').eq(6).click();
+        cy.get('.Bonuses_confirmButton__t5m2y').eq(0).click();
+        cy.window().its('store').invoke('getState').its('bonusReducer').its('bonuses').its('healer').its('unlocked').should('deep.equal', true);
+      });
+    });
+
+    describe('Swapping pokemon on your team.', () => {
+      it('Swapping the second pokemon on your team with the first one.', () => {
+        cy.get('.Options_option__h4v8l').eq(0).click();
+        cy.get('.TeamCard_pokemonName__nzyn0').eq(1).invoke('text').then((pokemonName) => {
+          cy.get('.TeamCard_swapButton__itTHd').eq(0).click();
+          cy.get('.TeamCard_pokemonName__nzyn0').eq(0).invoke('text').should('deep.equal', pokemonName);
+        });
       });
     });
   });
