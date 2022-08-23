6388f0b1b22ca865696c64ee9efef4f513ceb8fa
Jeffery Xie <jeffxie27@outlook.com>
2022-08-23 16:30:43 -0600

    Added a cache header.

--- next.config.js
+++ next.config.js
@@ -10,7 +10,20 @@ const nextConfig = {
       exportFolderPath: "out",
       quality: 75,
     },
-    minimumCacheTTL: 60
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|webp)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
   },
   env: {
     storePicturesInWEBP: true,
