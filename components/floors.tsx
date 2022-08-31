613b5f335c76be21373b3c3062a6e65f4dae504c
Jeffery Xie <jeffxie27@outlook.com>
2022-08-31 15:48:14 -0600

    Added bonus points and currency rewards.

--- components/floors.tsx
+++ components/floors.tsx
@@ -16,6 +16,8 @@ const Floors = () => {
   const dispatch = useDispatch();
   const floor = useSelector((state: any) => {return state.gameReducer});
   const biomes = useSelector((state: any) => {return state.biomeReducer.biomes});
+  const currency = useSelector((state: any) => {return state.gameReducer.currency});
+  const bonusPoints = useSelector((state: any) => {return state.bonusReducer.bonusPoints});
 
   const setFloor = (newFloor: number) => {
     if (newFloor === floor.currentFloor) return;
@@ -34,6 +36,16 @@ const Floors = () => {
       dispatch(allActions.biomeActions.setActiveBiome(biomes[Math.floor(floor.currentFloor / 5)]));
       return;
     } 
+
+    // Give players extra bonus points and gold after every 5th and 10th floor
+    if ((floor.currentFloor % 6 === 0 || floor.currentFloor % 11 === 0) && floor.currentFloor > floor.highestFloor) {
+      const goldReward = Math.ceil(floor.currentFloor ** 2.5);
+      const bpReward = Math.floor(floor.currentFloor ** 0.5);
+
+      dispatch(allActions.gameActions.setCurrency(currency + goldReward));
+      dispatch(allActions.bonusActions.setBonusPoints(bonusPoints + bpReward));
+      dispatch(allActions.alertActions.addAlert(`For beating floor ${floor.currentFloor}, you earned ${goldReward} and ${bpReward}BP!`));
+    }
     
     // Create a new biome every 5th floor
     if (Math.floor(floor.currentFloor / 5) >= biomes.length) {
@@ -42,7 +54,7 @@ const Floors = () => {
       // Re-roll once if we have already seen this biome for more variety
       if (biomes.includes(newBiome)) {
         newBiome = Object.keys(biomeList)[Math.floor(Math.random() * (Object.keys(biomeList).length - 1))];
-      }
+      }      
 
       dispatch(allActions.biomeActions.setBiomes([...biomes, newBiome]));
       dispatch(allActions.biomeActions.setActiveBiome(newBiome));
